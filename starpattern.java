01b459ab121effd7c398387962a6837a7d476441
Samruddhi1710 <samruddhinimbalkar3@gmail.com>
2024-07-29 22:24:25 +0530

    intial commit

---
+++ starpattern.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class starpattern {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    try{
+        int num=sc.nextInt();
+        System.out.print("The number is "+ num);
+    }finally{
+        sc.close();
+    }
+ }
+}
