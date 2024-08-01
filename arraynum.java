bb72bc06560a54b8e335d4b5f8bf12926103c153
Samruddhi1710 <samruddhinimbalkar3@gmail.com>
2024-08-01 23:01:43 +0530

    Initial commit

---
+++ arraynum.java
@@ -0,0 +1,13 @@
+public class arraynum {
+    public static void main (String[] arg){
+        Scanner sc= new Scanner(System.in);
+        try{
+            
+
+        }finally{
+            sc.close();
+        }
+
+
+    }
+}
