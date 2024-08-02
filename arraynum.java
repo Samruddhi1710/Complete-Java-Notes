9c772e96d911b22880a1026722ea4a09a22ffa8f
Samruddhi1710 <samruddhinimbalkar3@gmail.com>
2024-08-02 23:18:03 +0530

    Initial commit

--- arraynum.java
+++ arraynum.java
@@ -1,8 +1,12 @@
+import java.util.*;
 public class arraynum {
     public static void main (String[] arg){
         Scanner sc= new Scanner(System.in);
         try{
-            
+            int[] array = new int[10];
+            for(int i=0;i< array.length;i++){
+                System.out.println(array[i]);
+            }
 
         }finally{
             sc.close();
